07475310dbc30e3fb3509083e9edc4b228ed513e
Alexis <alexispaiement25@gmail.com>
2026-02-17 10:56:32 -0500

    Le main de l'interface graphique

--- interface_graphique.py
+++ interface_graphique.py
@@ -4,14 +4,30 @@ import pygame
 
 
 pygame.init()
-screen = pygame.display.set_mode(640,640)
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((1280,640))
+##clock = pygame.Clock()
 
 
 ## boucle de running
+
 running = True
+
 while running:
-    for event in pygame.get():
+
+    ##dessins
+
+
+    pygame.draw.rect(screen, color = "green", rect = (100, 200, 50, 50))
+
+
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.flip()
+    ##clock.tick(60)
+
+
 pygame.quit()
+
+## dessins
+
