1f3df5bcfd26f4cb110be06336d3e7faba4e860d
Bisqu1 <mr.mathisrobinet@gmail.com>
2026-02-24 10:59:26 -0500

    WIP pykt apprentissage BUG

--- interactivite.py
+++ interactivite.py
@@ -1,63 +1,133 @@
 import pygame
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QMainWindow, QLabel, QVBoxLayout
 from pygame import MOUSEWHEEL, MOUSEBUTTONDOWN
-
+import sys
 import loi_physique
 import random
 
-#Variable équation puissance théorique central hydro (Test)
-# P= densite * gravite * debit * hauteur * rendement
-rendement=5
-densite=4
-gravite=3
-debit=2
-hauteur=0
-
-#print(loi_physique.equation(rendement,densite,gravite,debit,hauteur,))
 
 
-#parametre affichage
-pygame.init()
-screen_width= 1280
-screen_height= 720
-screen=pygame.display.set_mode([screen_width, screen_height])
+class Interface(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
 
-clock = pygame.time.Clock()
-running = True
-centre_pos = (random.randrange(screen_width), random.randrange(screen_height))
-rayon= random.randint(20,50)
-dt = 0
-rectangle= pygame.Rect(random.randrange(screen_width),random.randrange(screen_height), random.randint(20,100),random.randint(20,100) )
-couleur= (random.randrange(255),random.randrange(255), random.randrange(255))
-#boucle affichage
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        #evenement clique souris
-        elif event.type == MOUSEBUTTONDOWN:
-            print(event)
+        self.setWindowTitle("Interface")
+        self.setGeometry(50, 50, 800, 600)
+        self.UIcomponents()
+        self.show()
 
-            #cliquer sur objet
-            if event.button == 1:
-                if rectangle.collidepoint(event.pos):
-                    couleur= "red"
-        #detecte mouvement souris
-        elif event.type == pygame.MOUSEMOTION:
-            souris_pos= pygame.mouse.get_pos()
-            print(souris_pos)
+        window =QMainWindow()
 
 
 
-    #visuel
-    screen.fill("purple")
-    pygame.draw.rect(screen, couleur,rectangle )
-
-    #interactivite
+        #layout = QVBoxLayout()
+        #layout.addWidget(label)
+        #self.setLayout(layout)
 
 
-    pygame.display.flip()
 
-    dt = clock.tick(60)/1000
-pygame.quit()
+
+
+    label = QLabel("LAAAABEEEEEEEEL")
+    label.setGeometry(50, 100, 150, 30)
+
+
+
+    def UIcomponents(self):
+        self.spinbox = QtWidgets.QSpinBox(self)
+        self.spinbox.setGeometry(50, 50, 1500, 300)
+        self.spinbox.valueChanged.connect(self.show_result())
+
+        #self.layout = QtWidgets.QVBoxLayout(self)
+#
+        #self.layout.addWidget(self.spinbox)
+
+
+
+
+    def show_result(self):
+        # getting current value
+        value = self.spinbox.value()
+        # setting value of spin box to the label
+        self.label.setText("Value : " + str(value))
+
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = Interface()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
+
+
+
+
+
+
+
+
+
+
+##Variable équation puissance théorique central hydro (Test)
+## P= densite * gravite * debit * hauteur * rendement
+#rendement=5
+#densite=4
+#gravite=3
+#debit=2
+#hauteur=0
+#
+##print(loi_physique.equation(rendement,densite,gravite,debit,hauteur,))
+#
+#
+##parametre affichage
+#pygame.init()
+#screen_width= 1280
+#screen_height= 720
+#screen=pygame.display.set_mode([screen_width, screen_height])
+#
+#clock = pygame.time.Clock()
+#running = True
+#centre_pos = (random.randrange(screen_width), random.randrange(screen_height))
+#rayon= random.randint(20,50)
+#dt = 0
+##rectangle= pygame.Rect(random.randrange(screen_width),random.randrange(screen_height), random.randint(20,100),random.randint(20,100) )
+#rectangle= pygame.Rect(screen_width/2,screen_height/2, random.randint(20,100),random.randint(20,100) )
+#couleur= (random.randrange(255),random.randrange(255), random.randrange(255))
+##boucle affichage
+#while running:
+#    # poll for events
+#    # pygame.QUIT event means the user clicked X to close your window
+#    for event in pygame.event.get():
+#        if event.type == pygame.QUIT:
+#            running = False
+#        #evenement clique souris
+#        elif event.type == MOUSEBUTTONDOWN:
+#            print(event)
+#
+#            #cliquer sur objet
+#            if event.button == 1:
+#                if rectangle.collidepoint(event.pos):
+#                    couleur= "red"
+#        #detecte mouvement souris
+#        elif event.type == pygame.MOUSEMOTION:
+#            souris_pos= pygame.mouse.get_pos()
+#            print(souris_pos)
+#
+#
+#
+#    #visuel
+#    screen.fill("purple")
+#    pygame.draw.rect(screen, couleur,rectangle )
+#
+#    #interactivite
+#
+#
+#    pygame.display.flip()
+#
+#    dt = clock.tick(60)/1000
+#pygame.quit()
--- test.py
+++ test.py
@@ -1,43 +1,121 @@
 # Example file showing a circle moving on screen
-import pygame
+#import pygame
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
+import random
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("green")
+class MainWindow(QWidget):
+   def __init__(self):
+      super().__init__()
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+      self.setWindowTitle('PyQt Label Widget')
+      self.setGeometry(100, 100, 320, 210)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+      label = QLabel('This is a QLabel widget')
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+      layout = QVBoxLayout()
+      layout.addWidget(label)
+      self.setLayout(layout)
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+      self.show()
 
-pygame.quit()
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   window = MainWindow()
+   sys.exit(app.exec())
+
+#from PySide6.QtWidgets import QApplication, QMainWindow, QSpinBox
+#import sys
+#
+#app = QApplication(sys.argv)
+#window = QMainWindow()
+#window.setGeometry(100, 100, 600, 400) # Sets main window position and dimension
+#
+#spin_box = QSpinBox(window) # Parent is the main window
+#spin_box.setGeometry(50, 50, 1500, 300) # Sets the spin box at x=50, y=50 with width=150, height=30
+#
+#window.show()
+#sys.exit(app.exec_())
+
+
+
+#################TEST-INI-PYQT###############################
+#class MyWidget(QtWidgets.QWidget):
+#    def __init__(self):
+#        super().__init__()
+#
+#        #self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+#        #self.button = QtWidgets.QPushButton("Click me!")
+#        #self.text = QtWidgets.QLabel("Hello World",
+#        #                             alignment=QtCore.Qt.AlignCenter)
+#        #self.butotn= QtWidgets.QSpinBox()
+#
+#        #self.layout = QtWidgets.QVBoxLayout(self)
+#        #self.layout.addWidget(self.text)
+#        #self.layout.addWidget(self.button)
+#        #self.layout.addWidget(self.butotn)
+#        #self.button.clicked.connect(self.magic)
+#
+#    @QtCore.Slot()
+#    def magic(self):
+#        self.text.setText(random.choice(self.hello))
+#
+#
+#if __name__ == "__main__":
+#    app = QtWidgets.QApplication([])
+#
+#    widget = MyWidget()
+#    widget.resize(800, 600)
+#    widget.show()
+#
+#    sys.exit(app.exec())
+
+
+######################PYGAME###################################
+#
+## pygame setup
+#pygame.init()
+#screen = pygame.display.set_mode((1280, 720))
+#clock = pygame.time.Clock()
+#running = True
+#dt = 0
+#
+#player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+#
+#while running:
+#    # poll for events
+#    # pygame.QUIT event means the user clicked X to close your window
+#    for event in pygame.event.get():
+#        if event.type == pygame.QUIT:
+#            running = False
+#
+#    # fill the screen with a color to wipe away anything from last frame
+#    screen.fill("green")
+#
+#    pygame.draw.circle(screen, "red", player_pos, 40)
+#
+#    keys = pygame.key.get_pressed()
+#    if keys[pygame.K_w]:
+#        player_pos.y -= 300 * dt
+#    if keys[pygame.K_s]:
+#        player_pos.y += 300 * dt
+#    if keys[pygame.K_a]:
+#        player_pos.x -= 300 * dt
+#    if keys[pygame.K_d]:
+#        player_pos.x += 300 * dt
+#
+#    # flip() the display to put your work on screen
+#    pygame.display.flip()
+#
+#    # limits FPS to 60
+#    # dt is delta time in seconds since last frame, used for framerate-
+#    # independent physics.
+#    dt = clock.tick(60) / 1000
+#
+#pygame.quit()
