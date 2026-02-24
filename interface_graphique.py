3a92a7dfe9ddae298bdacf0eb72d2c7a0cb1fe2a
Alexis <alexispaiement25@gmail.com>
2026-02-24 10:48:02 -0500

    WED

--- interface_graphique.py
+++ interface_graphique.py
@@ -1,33 +1,57 @@
-import pygame
-
-##crée une fenêtre
-
-
-pygame.init()
-screen = pygame.display.set_mode((1280,640))
-##clock = pygame.Clock()
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtWidgets import QToolButton, QLineEdit, QApplication, QPushButton
+import loi_physique as loi
 
 
-## boucle de running
 
-running = True
+##crée une classe widget
 
-while running:
+##class MyWidget(QtWidgets.QWidget):
 
-    ##dessins
+    ##def __init__(self):
+        ##super().__init__()
 
+        ##self.button = QtWidgets.QPushButton("Click me!")
+     ##self.text = QtWidgets.QLabel("Hello World",
+                                 ##alignment=QtCore.Qt.AlignCenter)
 
-    pygame.draw.rect(screen, color = "green", rect = (100, 200, 50, 50))
-
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
-    ##clock.tick(60)
+       ## boutton_demarree = QToolButton()
+       ## line_edit = QLineEdit()
+      ##  boutton_demarree.clicked.connect(line_edit.clear)
 
 
-pygame.quit()
 
-## dessins
+      ##  self.layout = QtWidgets.QVBoxLayout(self)
+       ## self.layout.addWidget(self.text)
+      ##  self.layout.addWidget(self.button)
 
+
+
+##if __name__=="__main__":
+   ## app = QtWidgets.QApplication([])
+   ## widget = MyWidget()
+  ##  widget.resize(800, 600)
+##widget.show()
+
+   ## sys.exit(app.exec())
+
+
+
+## boutton démarrer
+
+boutton_demarree = QPushButton("Démarrer la simulation!")
+if boutton_demarree.clicked :
+
+   résultat = loi.equation(r=3,p=5,d=4,h=3)
+   affichage_resultat_equation =
+
+
+
+app = QApplication()
+button = QPushButton("Call function")
+button.clicked.connect(function)
+button.show()
+boutton_demarree.show()
+sys.exit(app.exec())
