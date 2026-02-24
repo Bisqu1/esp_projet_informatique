0cb4fa1b4d5eed1c9fbfc5b8fe66c9f5604782fa
Rayen232 <rayan.rouz@gmail.com>
2026-02-24 09:29:17 -0500

    WIP

--- main.py
+++ main.py
@@ -17,20 +17,21 @@ masse_volumique=1000
 g=9.81
 hauteur=20
 
-débit=input("Quelle est le débit d'eau")
+rendement=float(input("Quelle est le rendement de la centrale"))
 
-rendement=input("Quelle est le rendement de la centrale")
-
-if 0.9<rendement or rendement<0.6:
+while 0.9 < rendement or 0.6 > rendement:
     print("Le rendement de la centrale doit être compris entre 0.6 et 0.9")
-    rendement=input("Quelle est le rendement de la centrale")
-else:
-    p = loi_physique.equation(rendement, masse_volumique, g, débit, hauteur)
-    print(p)
+    rendement=float(input("Quelle est le rendement de la centrale"))
 
+débit=float(input("Quelle est le débit d'eau"))
 
-p=loi_physique.equation(rendement,masse_volumique,g,débit,hauteur)
-print(p)
+while 10 > débit or 1000 < débit:
+    print("Le débit de la centrale doit être compris entre 10 et 1000 m³/s")
+    débit = float(input("Quelle est le débit d'eau"))
+
+p = loi_physique.equation(rendement, masse_volumique, g, débit, hauteur)
+print(p/1000,"kW")
+if p/1000 >
 
 #graphique des valeurs de P
 
