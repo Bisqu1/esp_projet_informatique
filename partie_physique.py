3013f9f63ba3ab21bbba3db27255cdbf37f0bc15
mathis <mr.mathisrobinet@gmail.com>
2026-03-11 15:22:54 -0400

    Refactor run_centrale function parameters and logic
    correction:
    - ordre des arguments de la fonction
    - diviser p par 1 000 000 pour convertir en MW (sinon crash a cause graduation graphique)
    - mettre les input en commentaire (sinon conflit entre variables Q et eta ---> valeur de input VS valeur de spinbox dans fonction interface dans interactivite.py)

--- partie_physique.py
+++ partie_physique.py
@@ -1,7 +1,8 @@
 ##############################################################################################################
 # Crée par Mathis Robinet, Rayen Rouz, Alexis Paiement
 # Crée le 2026-02-17
-#420-ESP-MA INTÉGRATION DES ACQUIS EN SCIENCES DE LA NATURE - PROJET EN INFORMATIQUE: Simulation d'une central hydroélectrique
+# 420-ESP-MA INTÉGRATION DES ACQUIS EN SCIENCES DE LA NATURE - PROJET EN INFORMATIQUE: Simulation d'une central hydroélectrique
+#  partie-physique.py
 ##############################################################################################################
 import numpy
 import matplotlib.pyplot as plt
@@ -9,27 +10,27 @@ import csv
 import loi_physique
 import os
 
-def run_centrale(eta, Q, masse_volumique=1000, g=9.81, h=20):
+def run_centrale(Q, h, eta, masse_volumique=1000, g=9.81):
     #rendement = float(input("Quelle est le rendement de la centrale: "))
-    while not (0.6 <= eta <= 0.9):
-        print("Le rendement de la centrale doit être compris entre 0.6 et 0.9")
-        eta = float(input("Quelle est le rendement de la centrale: "))
-
-    #débit = float(input("Quelle est le débit d'eau: "))
-    while not (10 <= Q <= 1000):
-        print("Le débit de la centrale doit être compris entre 10 et 1000 m³/s")
-        Q = float(input("Quelle est le débit d'eau: "))
-
-    #Calcul
-    p = loi_physique.calculer_puissance(eta, masse_volumique, g, Q, h)
-    print(p / 1000, "kW")
-
-    if p / 1000 < 72000:
-        print("Il n'y a pas assez d'énergie pour alimenter tous les habitants du village")
-    else:
-        print(f"Il y a {p / 1000 - 72000} kW en surplus")
-
-    #Fichier CSV
+    #while not (0.6 <= eta <= 0.9):
+    #    print("Le rendement de la centrale doit être compris entre 0.6 et 0.9")
+    #    eta = float(input("Quelle est le rendement de la centrale: "))
+#
+    ##débit = float(input("Quelle est le débit d'eau: "))
+    #while not (10 <= Q <= 1000):
+    #    print("Le débit de la centrale doit être compris entre 10 et 1000 m³/s")
+    #    Q = float(input("Quelle est le débit d'eau: "))
+##
+    ##Calcul
+    p = loi_physique.calculer_puissance(Q, h, eta, masse_volumique, g)/1_000_000
+    #print(p / 1000, "kW")
+#
+    #if p / 1000 < 72000:
+    #    print("Il n'y a pas assez d'énergie pour alimenter tous les habitants du village")
+    #else:
+    #    print(f"Il y a {p / 1000 - 72000} kW en surplus")
+#
+    ##Fichier CSV
     filename = "scatter_power.csv"
     powers = []
 
@@ -53,3 +54,5 @@ def run_centrale(eta, Q, masse_volumique=1000, g=9.81, h=20):
         for power_val in powers:
             writer.writerow([power_val])
     return powers
+
+
