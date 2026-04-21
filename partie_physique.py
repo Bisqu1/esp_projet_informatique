1943e3cc7051c8051c4cd238d22ef21457c0136a
mathis <mr.mathisrobinet@gmail.com>
2026-04-21 10:37:40 -0400

    Modify afficher_graphique to include puissance parameter

--- partie_physique.py
+++ partie_physique.py
@@ -79,35 +79,42 @@ class AnalyseDonnees(FigureCanvasQTAgg):
             writer = csv.writer(csvfile)
             writer.writerow([p])
 
-    def afficher_graphique(self, consommation, perte):
+    def afficher_graphique(self, consommation, perte, puissance):
         self.x = list(range(1, len(self.powers) + 1))
+        self.ax2.clear()
 
         #perte puissance
         self.ax2.scatter(self.x, self.powers_pertes, s=10, color='orange', zorder=3, label=f"Pertes ({perte: .2f} MW)")
         self.ax2.set_ylabel("Pertes (MW)", fontsize=8, color='orange')
-        self.ax2.plot(self.x,self.powers_pertes, linestyle="--", marker="*",color="y")
+        self.ax2.yaxis.set_label_position("right")
+        self.ax2.yaxis.tick_right()
+        #self.ax2.plot(self.x,self.powers_pertes, linestyle="--", marker="*",color="y")
 
         #puissance
         couleurs = ['green' if p >= consommation else 'red' for p in self.powers]
         self.ax.clear()
-        self.ax.scatter(self.x, self.powers, s=10, color=couleurs, zorder=3, label="Puissance (MW)")
-        self.ax.set_xlabel("Numéro de run", fontsize=8)
+        self.ax.scatter(self.x, self.powers, s=10, color=couleurs, zorder=3, label=f"Puissance ({puissance: .2f}MW)")
+        self.ax.set_xlabel("Numéro de simulation", fontsize=8)
         self.ax.set_ylabel("Puissance (MW)", fontsize=8)
-        self.ax.set_title("Puissance par run", fontsize=10)
+        self.ax.set_title("Puissance par simulation", fontsize=10)
         self.ax.minorticks_on()
         self.ax.set_xticks(self.x)
         # if max(powers)> 0:
         # print(f"powers max: {max(self.powers)}")
         # print(f"nbr ticks: {len(np.arange(0, max(powself.powersers), 200))}")
-        self.y = np.arange(0, max(self.powers), 200)
+
+        # graduation: 0, max(self.powers, self.powers_pertes, consommation), 200
+        #self.y = np.arange(0, max(self.powers), 200)
+        self.y = np.arange(0, max(max(self.powers), max(self.powers_pertes), consommation), 200)
         self.ax.set_yticks(self.y)
         # self.xaxis.set_minor_locator(AutoMinorLocator())
-        self.ax.plot(self.x,self.powers, linestyle="-", marker=".",color="c" )
+        #self.ax.plot(self.x,self.powers, linestyle="-", marker=".",color="c" )
 
         #consommation
         self.ax.axhline( consommation, color='red', linestyle='--', linewidth=1, label=f"Consommation ({consommation} MW)")
 
         self.ax.legend(fontsize=7)
+        self.ax2.legend(fontsize=7)
         self.ax.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.8)
         self.ax.grid(which="minor", linestyle="-", linewidth=.5, alpha=0.7)
         self.ax.set_axisbelow(True)
